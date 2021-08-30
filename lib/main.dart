dfa5f369352891c314114775b2a7caa7f413b25c
HelloWorld183L <lharper970@outlook.com>
2021-08-30 17:27:40 +0100

    Created UI code for activities route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
+const backgroundColor = Color.fromRGBO(22, 25, 37, 1);
+const textColor = Color.fromRGBO(22, 25, 37, 1);
+const greenColor = Color.fromRGBO(96, 184, 61, 1);
+const progressColor = Color.fromRGBO(230, 115, 15, 1);
+const foregroundColor = Color.fromRGBO(48, 51, 61, 1);
+
+const textStyle = const TextStyle(color: textColor);
 
 void main() => runApp(MyApp());
 
@@ -7,93 +14,149 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup name generator',
+      title: 'Skill tree',
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: backgroundColor,
+        textTheme: const TextTheme(bodyText2: textStyle, bodyText1: textStyle)
       ),
-      home: RandomWords(),
+      home: ActivitiesRoute()
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ActivitiesRoute extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _ActivitiesRouteState createState() => _ActivitiesRouteState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
+class _ActivitiesRouteState extends State<ActivitiesRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        title: const Text('Activities'),
+      ),
+      floatingActionButton:  FloatingActionButton(
+        child: const Icon(Icons.add),
+        backgroundColor: foregroundColor,
+        onPressed: _addActivity,
+      ),
+
+      body: _displayActivities(),
+      drawer: _createDrawer()
+    );
+  }
+
+  Widget _createDrawer() {
+    return Drawer(
+      child: ListView(
+        children: const <Widget>[
+          ListTile(
+            leading: Icon(Icons.computer),
+            title: Text('Activities'),
+          ),
+          ListTile(
+            leading: Icon(Icons.piano),
+            title: Text('Skills'),
+          ),
+          ListTile(
+            leading: Icon(Icons.auto_graph),
+            title: Text('Statistics'),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ]
+      )
+    );
+  }
+
+  Widget _displayActivities() {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, i) {
+                return Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  height: 94,
+                  width: double.maxFinite,
+                  child: Card(
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, top: 5),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              activityName(),
+                              Spacer(),
+                              activityXPIncrease(),
+                              SizedBox(
+                                width: 10,
+                              ),
+                            ],
+                          ),
+                          addXPTxt()
+                        ],
+                      )
+                    )
+                  )
+                );
+              }
+            )
+          )
         ],
-      ),
-      body: _buildSuggestions(),
+      )
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
+  Widget addXPTxt() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        TextButton(
+          child: const Text('Add XP'),
+          onPressed: _addXP,
+        ),
+      ],
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final tiles = _saved.map(
-        (WordPair pair) {
-          return ListTile(
-            title: Text(
-              pair.asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        },
-      );
-      final divided = tiles.isNotEmpty
-          ? ListTile.divideTiles(context: context, tiles: tiles).toList()
-          : <Widget>[];
+  Widget activityName() {
+    return Align(
+      alignment: Alignment.center,
+      child: RichText(
+        text: TextSpan(
+          text: 'Activity name (0/1000)',
+          style: TextStyle(color: textColor, fontSize: 20)
+        )
+      )
+    );
+  }
 
-      return Scaffold(
-        appBar: AppBar(title: Text('Saved Suggestions')),
-        body: ListView(children: divided),
-      );
-    }));
+  Widget activityXPIncrease() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: RichText(
+        text: TextSpan(
+          text: '+0 XP',
+          style: TextStyle(color: greenColor, fontSize: 16)
+        )
+      )
+    );
+  }
+
+  void _addXP() {
+
+  }
+
+  void _addActivity() {
+
   }
 }

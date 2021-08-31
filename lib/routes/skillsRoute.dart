10f05c10929856207b6ef04dd401f1fb33ae046a
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2021-08-31 18:08:41 +0100

    Implemented UI for skillsRoute
    Implemented the UI for the skills route. "Backend" code still needs to be done.

--- lib/routes/skillsRoute.dart
+++ lib/routes/skillsRoute.dart
@@ -23,7 +23,48 @@ class _SkillsRouteState extends State<SkillsRoute> {
   }
 
   Widget _displaySkills() {
-    return Container();
+    return GridView.builder(
+      primary: false,
+      padding: const EdgeInsets.all(15),
+      itemCount: 3,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (context, i) {
+        return Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.white70, width: 1),
+                borderRadius: BorderRadius.circular(10)),
+            child: Expanded(child: _createCardInkwell()));
+      },
+    );
+  }
+
+  Widget _createCardInkwell() {
+    return InkWell(
+        splashColor: foregroundColor,
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [Icon(Icons.air, size: 50)],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              Spacer(),
+              Row(
+                children: [Text('PROGRAMMER (9001)')],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              LinearProgressIndicator(
+                value: 0.5,
+                color: progressColor,
+                backgroundColor: foregroundColor,
+              )
+            ],
+          ),
+        ));
   }
 
   void _addSkill() {}

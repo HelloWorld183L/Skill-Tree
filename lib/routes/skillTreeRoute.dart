084b8bc66aae20c393c61dd36c07c52e3d38a739
HelloWorld183L <46497907+HelloWorld183L@users.noreply.github.com>
2021-09-03 13:15:45 +0100

    Created scaffold
    Created scaffold and general UI code.

--- lib/routes/skillTreeRoute.dart
+++ lib/routes/skillTreeRoute.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'general.dart';
 
 class SkillTreeRoute extends StatefulWidget {
   @override
@@ -6,8 +7,21 @@ class SkillTreeRoute extends StatefulWidget {
 }
 
 class _SkillTreeRouteState extends State<SkillTreeRoute> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Skill tree'),
+        ),
+        body: _displayInputs(context),
+        drawer: createDrawer(context));
+  }
+
+  Widget _displayInputs(context) {
     return Container();
   }
+
+  void _addSkill() {}
 }
